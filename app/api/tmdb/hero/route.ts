4f594f71022b2188661e2978d7093d0ac7a3abb0
Akash Rajput <egao@Akashs-MacBook-Pro.local>
2026-07-20 20:52:25 +0530

    Fix TMDB authentication

--- app/api/tmdb/hero/route.ts
+++ app/api/tmdb/hero/route.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server";
 
-const API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = process.env.TMDB_READ_ACCESS_TOKEN;
 
 export async function GET() {
   try {
     const res = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
-    );
-
+  "https://api.themoviedb.org/3/trending/movie/week",
+  {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      accept: "application/json",
+    },
+  }
+);
     const data = await res.json();
 
     const randomMovie =
